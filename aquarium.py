a747ca9ffa338b65e22619b91106ebcf810e92a8
Bohdan Popovych <bohdan.popovych@masterofcode.com>
2016-12-24 22:50:56 +0200

    fixed auqarium quantity residents

--- aquarium.py
+++ aquarium.py
@@ -1,8 +1,17 @@
 from singleton import Singleton
 
 class Auqarium(metaclass=Singleton):
-    def __init__(self, max_residents):
-        self.max_residents = max_residents
+    MIN_RESIDENTS = 20
+    MAX_RESIDENTS = 100
+
+    def __init__(self, quantity_residents):
+        if quantity_residents < Auqarium.MIN_RESIDENTS:
+            raise Exception('Please enter quantity more {0}'.format(Auqarium.MIN_RESIDENTS))
+
+        elif quantity_residents > Auqarium.MAX_RESIDENTS:
+            raise Exception('Please enter quantity less {0}'.format(Auqarium.MAX_RESIDENTS))
+
+        self.quantity_residents = quantity_residents
         self.current_auqarium = []
 
     def add_resident(self, resident):
